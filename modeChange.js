ca38dea368e0a991f9e2e0166b4da1a8c964dc81
taner <tanerozer16@gmail.com>
2023-08-23 16:15:40 +0300

    edited mode

--- modeChange.js
+++ modeChange.js
@@ -1,15 +1,15 @@
 function toggleDarkMode() {
-    const body = document.querySelector('body');
-    const moonIcon = document.querySelector('.dark-mode-button i');
+  const body = document.querySelector("body");
+  const moonIcon = document.querySelector(".dark-mode-button i");
 
-    body.classList.toggle('dark-mode');
+  body.classList.toggle("dark-mode");
 
-    // İkonun durumuna göre sınıfını güncelle
-    if (body.classList.contains('dark-mode')) {
-        moonIcon.classList.remove('fa-moon');
-        moonIcon.classList.add('fa-sun');
-    } else {
-        moonIcon.classList.remove('fa-sun');
-        moonIcon.classList.add('fa-moon');
-    }
+  // İkonun durumuna göre sınıfını güncelle
+  if (body.classList.contains("dark-mode")) {
+    moonIcon.classList.remove("fa-moon");
+    moonIcon.classList.add("fa-sun");
+  } else {
+    moonIcon.classList.remove("fa-sun");
+    moonIcon.classList.add("fa-moon");
+  }
 }
